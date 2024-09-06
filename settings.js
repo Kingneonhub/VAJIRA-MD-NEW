48e2de0a8513ae935ce23445efcc7521c51759cc
Kingneonhub <osundekookikiola@gmail.com>
2024-09-06 19:34:23 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,8 +5,8 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? 'VAJIRA-MD=PMVWAC6I#ZXO-1BGFCdK3Jf-IgksTTSQ8HaIv-_LpC9xGDzVTiTU' : process.env.SESSION_ID,
-OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '263718036108' : process.env.OWNER_NUMBER,   
+SESSION_ID: process.env.SESSION_ID === undefined ? 'VAJIRA-MD=fI1yhTia#QWLb-IE7vuCrsqSvLo-Gig3hMVG7kK08sSDJWJgYW8c' : process.env.SESSION_ID,
+OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '263718313601' : process.env.OWNER_NUMBER,   
 ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'false' : process.env.ONLY_GROUP,
 ONLY_ME: process.env.ONLY_ME === undefined ? 'false' : process.env.ONLY_ME,
 AUTO_STATUS_READ:  process.env.AUTO_STATUS_READ  || false  ,    
